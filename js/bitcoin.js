47d5af0e1a99addbc54e8e0da525d80d1e653446
zibrata <Alexandre@macbook-pro-de-alexandre-4.home>
2017-03-13 01:42:09 +0100

    integration jQuery

--- js/bitcoin.js
+++ js/bitcoin.js
@@ -25,14 +25,15 @@ xmlhttp.send();
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        var myObj = this.responseText;
-        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("mininginfo").innerHTML = jsonPretty;
+      var myObj = JSON.parse(this.responseText);
+      document.getElementById("mininginfo").innerHTML = "\n blocks: "+myObj.blocks+"\n difficulty: "+myObj.difficulty+"\n chain: "+myObj.chain;
     }
 };
 xmlhttp.open("GET", "http://bitcoin.mubiz.com/mininginfo", true);
 xmlhttp.send();
 
+
+// A FAIRE LE PEER INFO
 
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
